d197907af96c2fe87ea2423d83dea9ad1ea6d1af
PascaIoana <pascaioana10@gmail.com>
2019-08-05 18:17:51 +0300

    feat: add method that builds scripts to create sql table to database

--- server/app/datastore/sqlite.ts
+++ server/app/datastore/sqlite.ts
@@ -1,40 +1,40 @@
 import { iDs } from "./iDs";
 export class Sqlite implements iDs {
-    createTable(tableName: string, tableColumns: Array<string> ) : string{
-        if (tableName === "" || !tableName){
+    createTable(tableName: string, tableColumns: Array<string>): string {
+        if (tableName === "" || !tableName) {
             throw new Error("Invalid table name");
         }
 
-        if(tableColumns === null || tableColumns.length <= 0){
+        if (tableColumns === null || tableColumns.length <= 0) {
             throw new Error("You need to provide at least one column for the table")
         }
 
         tableName = this.replaceInvalidCharacters(tableName);
 
         let query = "";
-        query = "CREATE TABLE " + tableName + " (";
-        
-        for(let i=0;i<tableColumns.length;i++){
+        query = "CREATE TABLE " + tableName + " ( ";
+
+        for (let i = 0; i < tableColumns.length; i++) {
             query += tableColumns[i] + " varchar,";
         }
 
-        query += query.substr(query.length -1);
-
+        query = query.substring(0, query.length - 1);
         query += ");"
-
         return query;
+
+
     }
 
 
-    insert(tableName : string, data: Array<string>) : void{
+    insert(tableName: string, data: Array<string>): void {
 
     };
 
-    delete(tableName: string) : void{
+    delete(tableName: string): void {
 
     }
 
-    replaceInvalidCharacters(literal: string) : string{
-          return literal.replace(/[^a-z0-9]/gi,'');  
-    }    
+    replaceInvalidCharacters(literal: string): string {
+        return literal.replace(/[^a-z0-9]/gi, '');
+    }
 }
