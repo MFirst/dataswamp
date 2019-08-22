396adb055b927dcf58eb7259c79b325c425b20db
Tomi Schvartz <tomi.schvartz@mobilefirst.ro>
2019-08-22 09:50:59 +0300

    added react-redux, reducers, redux, redux-logger packaeges

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
 
 const App: React.FC = () => {
+
+  const a = 10;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          {a}
         </p>
         <a
           className="App-link"
